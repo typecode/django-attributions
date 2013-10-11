b6ebca8b7c5df283fa1eea94ab4182d87144a5e8
Matt Briancon <matt.briancon@gmail.com>
2013-10-11 11:04:48 -0400

    Make installable with pip

---
+++ setup.py
@@ -0,0 +1,23 @@
+
+
+from setuptools import setup
+
+
+setup(
+    name='django-attributions',
+    version='0.0.1',
+    author='Matt Briançon',
+    author_email='matt@typecode.com',
+
+    url='https://github.com/typecode/django-attributions',
+    keywords='attribution generic relationship',
+    packages=['attributions', 'tests'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Framework :: Django',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+
+)
