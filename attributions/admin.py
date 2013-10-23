53d825ae3527c2e695ac99744d29769b75fdccf8
Matt Briancon <matt.briancon@gmail.com>
2013-10-23 13:29:06 -0400

    Limit Attribution ContentType choices in admin

--- attributions/admin.py
+++ attributions/admin.py
@@ -3,24 +3,33 @@
 from django.contrib import admin
 from django.contrib.contenttypes import generic
 from django.contrib.contenttypes.models import ContentType
+from django.db.models import Q
 
 
 class RootInline(admin.TabularInline):
     relatives = []
+    ct_field = 'content_type'
+    ct_fk_field = 'object_id'
 
     def __init__(self, *args, **kwargs):
         super(RootInline, self).__init__(*args, **kwargs)
 
-        self.relatives = [
-            ContentType.objects.get(app_label=rel[0], model=rel[1])
-            for rel in self.relatives
-        ]
+        q = Q()
+        if self.relatives:
+            for rel in self.relatives:
+                q |= Q(app_label=rel[0], model=rel[1])
 
-    def get_form(self, request, obj=None, **kwargs):
-        form = super(RootInline, self).get_form(request, obj, **kwargs)
-        # form.fields['content_type']. =
+            self.relatives = ContentType.objects.filter(q)
+        else:
+            self.relatives = None
 
-        return form
+    def get_formset(self, request, obj=None, **kwargs):
+        fs = super(RootInline, self).get_formset(request, obj=obj, **kwargs)
+
+        if self.relatives is not None:
+            fs.form.base_fields[self.ct_field].queryset = self.relatives
+
+        return fs
 
 
 class RelatedInline(generic.GenericTabularInline):
